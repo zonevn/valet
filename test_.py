943759fa6238b16bd2079b67f2810ae082a0a53e
zonevn <datazone.vn@gmail.com>
2018-10-23 08:51:27 +0700

    modified commands

--- test_.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from settings import ROOT_DIR
-
-
-def find_path(pkg):
-    path = os.path.join(ROOT_DIR, pkg)
-    assert os.path.isdir(path), 'Directory not found at {}'.format(pkg)
-    return path
